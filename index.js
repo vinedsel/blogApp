62445c5eb03bd06ba308082bd349b3cfbabac6f8
Raymond Gregory <vinedsel@gmail.com>
2020-01-05 20:32:39 -0600

    File Mod
    Modified file to accommodate file location requests.

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const server = http.createServer((req, res) =>{
+    if(req.url === '/about')
+        res.end('The About Page');
+    else if(req.url === '/contact')
+        res.end('The Contact Page');
+    else if(req.url === '/')
+        res.end('The Home Page');
+    else {
+        res.writeHead(404);
+        res.end('page not found');
+    }            
+})
+
+server.listen(3000);
+
