a4c3e781e128a3d4f53437b0ea05520fc245af29
Raymond Gregory <vinedsel@gmail.com>
2020-01-06 23:54:57 -0600

    Code Refactoring
    Conversion of the API to the Express way.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const http = require('http');
+// const http = require('http');
 const fs = require('fs');
 const homePage = fs.readFileSync('index.html');
 const aboutPage = fs.readFileSync('about.html');
 const contactPage = fs.readFileSync('contact.html');
 const notFoundPage = fs.readFileSync('404.html');
+const express = require('express');
+const app = express();
+app.listen(3000, ()=>(
+    console.log("App listening on port 3000")
+));
 
-const server = http.createServer((req, res) =>{
-    if(req.url === '/about')
-        res.end(aboutPage);
-    else if(req.url === '/contact')
-        res.end(contactPage);
-    else if(req.url === '/')
-        res.end(homePage);
-    else {
-        res.writeHead(404);
-        res.end(notFoundPage);
-    }            
-});
+// const server = http.createServer((req, res) =>{
+//     if(req.url === '/about')
+//         res.end(aboutPage);
+//     else if(req.url === '/contact')
+//         res.end(contactPage);
+//     else if(req.url === '/')
+//         res.end(homePage);
+//     else {
+//         res.writeHead(404);
+//         res.end(notFoundPage);
+//     }            
+// });
 
-server.listen(3000);
+// server.listen(3000);
 
