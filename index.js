e5c5ccb0c493b7dc6141e275f7cdfe1f79ed0f78
Raymond Gregory <vinedsel@gmail.com>
2020-01-12 18:40:04 -0600

    Added request handlers to file.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const notFoundPage = fs.readFileSync('404.html');
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.static('public'));
 
 app.listen(3000,()=>{
     console.log("App listening on port 3000");
