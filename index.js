96ddc7b9dc8885ce2f01af5cda6150e3b3789e86
Raymond Gregory <vinedsel@gmail.com>
2020-01-12 18:31:31 -0600

    Handlers
    Added request handlers.

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const contactPage = fs.readFileSync('contact.html');
 const notFoundPage = fs.readFileSync('404.html');
 const express = require('express');
 const app = express();
-app.listen(3000, ()=>(
-    console.log("App listening on port 3000")
-));
+const path = require('path');
+
+app.listen(3000,()=>{
+    console.log("App listening on port 3000");
+});
+
+
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'about.html'));
+});
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'contact.html'));
+});
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'));
+});
 
 // const server = http.createServer((req, res) =>{
 //     if(req.url === '/about')
